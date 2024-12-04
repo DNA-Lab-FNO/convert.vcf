28f516628f5fc2f5893b031f5e9d81d7b38aa7c5
gorgitko <fg-42@seznam.cz>
2024-12-04 19:50:41 +0100

    add VCF conversion CLI

---
+++ R/cli.R
@@ -0,0 +1,55 @@
+.get_vcf_convert_parsed_args <- function(args) {
+  parser <- argparse::ArgumentParser(description = "Aggregate and convert VCF files to user-friendly format, including FinalistDX Excel format")
+  parser$add_argument(
+    "--vc_tool",
+    default = "haplotypecaller",
+    type = "character",
+    choices = c("haplotypecaller", "deepvariant", "strelka"),
+    help = "Name of the variant calling tool that originally produced the VCF files [default: %(default)s]"
+  )
+  parser$add_argument(
+    "--vcf_annotation_tool",
+    default = "vep",
+    type = "character",
+    choices = c("vep", "common", "snpeff"),
+    help = "Name of the variant annotation tool that was used for the original VCF files ('common' is for generic VCFs) [default: %(default)s]"
+  )
+  parser$add_argument(
+    "--no_variant_filtering",
+    action="store_true",
+    help = "Skip variant filtering by FILTER column in VCF file"
+  )
+  # parser$add_argument(
+  #   "--output_type",
+  #   default = "finalist",
+  #   type = "character",
+  #   choices = c("original", "finalist"),
+  #   help = "Output type: 'finalist' is the FinalistDX Excel format, 'original' is just user-friendly parsed VCF"
+  # )
+  parser$add_argument(
+    "--output_file",
+    required = TRUE,
+    type = "character",
+    help = "Path to output CSV (.csv), Excel (.xlsx), or R binary object (.rds) file (will be determined based on the extension)"
+  )
+  parser$add_argument(
+    "vcf_files",
+    type = "character",
+    nargs = "+",
+    help = "Paths to VCF files"
+  )
+
+  parser$parse_args(args = args)
+}
+
+run_vcf_convert_cli <- function(args = commandArgs(TRUE)) {
+  args <- .get_vcf_convert_parsed_args(args)
+
+  cli::cli_alert_info("Input files ({length(args$vcf_files)}):")
+  cli::cli_li(items = glue("{{.file {args$vcf_files}}}"))
+
+  agg_df <- convert_vcf_files_to_finalist(args$vcf_files, filter_variants = !args$no_variant_filtering, vc_tool = args$vc_tool, vcf_annotation_tool = args$vcf_annotation_tool)
+
+  cli::cli_alert_info("Writing result to {.file {args$output_file}}")
+  write_output_file(agg_df, args$output_file)
+}
--- tests/testthat/test-sarek_vcf.R
+++ tests/testthat/test-sarek_vcf.R
@@ -1,5 +1,14 @@
 TEST_VCF_FILES_DIR <- fs::path("test_data/vcf")
+
+HAPLOTYPECALLER_VCF_GLOB <- "*.haplotypecaller.filtered_VEP.ann.vcf.gz"
+DEEPVARIANT_VCF_GLOB <- "*.deepvariant_VEP.ann.vcf.gz"
+STRELKA_VCF_GLOB <- "*.strelka.variants_VEP.ann.vcf.gz"
+
 EXPECTED_FINALIST_FILES_DIR <- fs::path("expected_data/finalist")
+
+.get_vcf_files <- function(run_name, glob) {
+  fs::dir_ls(TEST_VCF_FILES_DIR / run_name, glob = glob)
+}
 
 .load_expected_finalist_df <- function(vcf_files, run_name) {
   purrr::map_dfr(vcf_files, function(vcf_file) {
@@ -17,8 +26,8 @@ EXPECTED_FINALIST_FILES_DIR <- fs::path("expected_data/finalist")
     dplyr::arrange(SAMPLE_variant_key)
 }
 
-assert_finalist_df_equality <- function(run_name) {
-  vcf_files <- fs::dir_ls(TEST_VCF_FILES_DIR / run_name, glob = "*.haplotypecaller.filtered_VEP.ann.vcf.gz")
+assert_haplotypecaller_finalist_df_equality <- function(run_name) {
+  vcf_files <- .get_vcf_files(run_name, HAPLOTYPECALLER_VCF_GLOB)
   finalist_df <- convert_vcf_files_to_finalist(vcf_files, vc_tool = "haplotypecaller", vcf_annotation_tool = "vep") %>%
     .prepare_finalist_df()
 
@@ -39,10 +48,30 @@ assert_finalist_df_equality <- function(run_name) {
   expect_true(all.equal(expected_finalist_df, finalist_df))
 }
 
-test_that("Conversion to FinalistDX format works (old Sarek 3.1.* VCF/VEP format)", {
-  assert_finalist_df_equality("2021_09_MR-Mikro4")
+assert_finalist_csv_equal <- function(run_name, vc_tool, glob) {
+  cli::cli_alert_info("{run_name} / {vc_tool} / {glob}")
+  withr::with_tempfile("out_file", fileext = ".csv", code = {
+    run_vcf_convert_cli(c("--vc_tool", vc_tool, "--output_file", out_file, .get_vcf_files(run_name, glob)))
+    expect_snapshot_file(out_file, name = glue("{run_name}_{vc_tool}.csv"))
+  })
+}
+
+test_that("Conversion to FinalistDX format works (old Sarek 3.1.* VCF/VEP format, haplotypecaller)", {
+  assert_haplotypecaller_finalist_df_equality("2021_09_MR-Mikro4")
 })
 
-test_that("Conversion to FinalistDX format works (new Sarek 3.4.2 VCF/VEP format)", {
-  assert_finalist_df_equality("2024_05_24_Panel_run_2")
+test_that("Conversion to FinalistDX format works (new Sarek 3.4.2 VCF/VEP format, haplotypecaller)", {
+  assert_haplotypecaller_finalist_df_equality("2024_05_24_Panel_run_2")
+})
+
+test_that("Conversion to FinalistDX format works using CLI (old Sarek 3.1.* VCF/VEP format, all VC tools, CSV output)", {
+  run_name <- "2021_09_MR-Mikro4"
+  assert_finalist_csv_equal(run_name, "haplotypecaller", HAPLOTYPECALLER_VCF_GLOB)
+  assert_finalist_csv_equal(run_name, "deepvariant", DEEPVARIANT_VCF_GLOB)
+  assert_finalist_csv_equal(run_name, "strelka", STRELKA_VCF_GLOB)
+})
+
+test_that("Conversion to FinalistDX format works using CLI (new Sarek 3.4.2 VCF/VEP format, haplotypecaller, CSV output)", {
+  run_name <- "2024_05_24_Panel_run_2"
+  assert_finalist_csv_equal(run_name, "haplotypecaller", HAPLOTYPECALLER_VCF_GLOB)
 })
