07189f8b4704d91f5bb28a47b3c729e09b9aee6d
gorgitko <fg-42@seznam.cz>
2025-01-15 22:16:19 +0100

    update function docs, add README

---
+++ R/roxygen_helpers.R
@@ -0,0 +1,7 @@
+#' @param vcf_annotation_tool A character scalar: which variant annotation tool was used:
+#' - `vep`: Variant Effect Predictor
+#' - `snpeff`: snpEff
+#' - `common`: a generic VCF
+#'
+#' @name vcf_annotation_tool_param
+NULL
