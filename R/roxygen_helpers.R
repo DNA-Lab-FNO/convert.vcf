da8d1c2186e0baa72dc78d213578067fcafbfe06
gorgitko <fg-42@seznam.cz>
2025-01-19 12:28:41 +0100

    add MANE Select and MANE Clinical Plus to Finalist Excel

--- R/roxygen_helpers.R
+++ R/roxygen_helpers.R
@@ -5,3 +5,14 @@
 #'
 #' @name vcf_annotation_tool_param
 NULL
+
+#' @param vc_tool A character scalar: variant calling tool used to produce the VCF files.
+#' This influences behaviour [calculate_variant_stats()].
+#'
+#' @name vc_tool_param
+NULL
+
+#' @param filter_variants A logical scalar: if `TRUE`, apply [filter_vcf()]
+#'
+#' @name filter_variants_param
+NULL
