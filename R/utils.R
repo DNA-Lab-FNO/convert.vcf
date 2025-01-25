f26fc1a168424c4b2711d8149417d9a0ca0df892
gorgitko <fg-42@seznam.cz>
2025-01-25 10:49:12 +0100

    overwrite installed script by default

--- R/utils.R
+++ R/utils.R
@@ -27,13 +27,13 @@ write_output_file <- function(df, output_file) {
 }
 
 #' @export
-install_cli <- function(out_dir, script = c("convert_vcf.R")) {
+install_cli <- function(out_dir, script = c("convert_vcf.R"), overwrite = TRUE) {
   rlang::arg_match(script)
   out_dir <- fs::path(out_dir)
 
   script_file <- fs::path(system.file("scripts", package = "convert.vcf", mustWork = TRUE)) / script
   cli::cli_alert_info("Copying {.file {script_file}} to {.file {out_dir}}")
-  fs::file_copy(script_file, out_dir)
+  fs::file_copy(script_file, out_dir, overwrite = overwrite)
   fs::file_chmod(out_dir / script, "+x")
   cli::cli_alert_success("Done")
 }
