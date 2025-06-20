dd57b83046f3dca91b13211884a43c480a4e7b00
Jiri Novotny <15315342+gorgitko@users.noreply.github.com>
2025-06-20 18:35:51 +0200

    fix parse_genotype tests (#21)

--- tests/testthat/test-parse_genotype.R
+++ tests/testthat/test-parse_genotype.R
@@ -9,15 +9,15 @@ test_that(".parse_genotype parses basic genotypes correctly", {
 })
 
 test_that(".parse_genotype parses multi-allelic genotypes correctly", {
-  expect_equal(.parse_genotype("0/1/2"), "MULTI_HET")
-  expect_equal(.parse_genotype("0|2|3"), "MULTI_HET")
-  expect_equal(.parse_genotype("1/2/3"), "MULTI_ALT")
-  expect_equal(.parse_genotype("2|3|4"), "MULTI_ALT")
+  expect_equal(.parse_genotype("0/1/2"), "HET_multi")
+  expect_equal(.parse_genotype("0|2|3"), "HET_multi")
+  expect_equal(.parse_genotype("1/2/3"), "HET_alt_multi")
+  expect_equal(.parse_genotype("2|3|4"), "HET_alt_multi")
 })
 
 test_that(".parse_genotype handles missing and unknown cases", {
   expect_equal(.parse_genotype("."), NA_character_)
   expect_equal(.parse_genotype(NA_character_), NA_character_)
   expect_equal(.parse_genotype("0/5"), "HET")
-  expect_equal(.parse_genotype("5/5/5"), "MULTI_ALT")
+  expect_equal(.parse_genotype("5/5/5"), "HET_alt_multi")
 })
